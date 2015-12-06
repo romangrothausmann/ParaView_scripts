1797ab852152260f7eab10f869b89150fc8d5f3f
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2015-12-06 17:57:08 +0100

    first commit for ParaView scripts

---
+++ template.py
@@ -0,0 +1,68 @@
+#!/usr/bin/pvpython
+
+from paraview.simple import *
+
+
+def main():
+    import sys       # to get command line args
+    import argparse  # to parse options for us and print a nice help message
+
+    argv = sys.argv
+
+    if __file__ in argv:
+        argv = argv[argv.index(__file__) + 1:]  # get all args after __file__
+
+
+    usage_text =  "Run ParaView in background mode with this script:"
+    usage_text += "  pvpython " + __file__ + " [options]"
+
+    parser = argparse.ArgumentParser(description=usage_text)
+
+    parser.add_argument("-i", "--input", dest="input", metavar='FILE', required=True, help="Input path contained in a text file.")
+    parser.add_argument("-o", "--output", dest="output", metavar='FILE', required=True, help="Output file to save the ParaView state in (*.pvsm)")
+
+    args = parser.parse_args(argv)
+
+    if not argv:
+        parser.print_help()
+        return
+
+    if not args.input:
+       print('Need an input file')
+       parser.print_help()
+       sys.exit(1)
+
+    if not args.output:
+       print('Need an output file')
+       parser.print_help()
+       sys.exit(1)
+
+    ## read pvsm
+    #servermanager.LoadState(args.input)
+
+    ## read a vtp
+    reader = XMLPolyDataReader(FileName=args.input)
+
+    sph = Sphere()
+
+    if args.output:
+        try:
+            f = open(args.output, 'w')
+            f.close()
+            ok = True
+        except:
+            print("Cannot save to path %r" % save_path)
+
+            import traceback
+            traceback.print_exc()
+
+        if ok:
+            servermanager.SaveState(args.output)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
