1b5d25b51f5981e9711fe38b151da1e9af02d448
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2017-09-04 14:01:38 +0200

    taking template.py as base for pvsm-multi.py

---
+++ pvsm-multi.py
@@ -0,0 +1,74 @@
+#!pvpython
+
+## API: http://www.paraview.org/ParaView/Doc/Nightly/www/py-doc/index.html
+
+
+import paraview.simple as pvs
+
+
+def main():
+    import sys       # to get command line args
+    import argparse  # to parse options for us and print a nice help message
+
+    argv = sys.argv
+
+    if __file__ in argv:
+        argv = argv[argv.index(__file__) + 1:]  # get all args after __file__
+
+
+    usage_text =  "Run ParaView in background mode with this script:"
+    usage_text += "  pvpython " + __file__ + " [options]"
+
+    parser = argparse.ArgumentParser(description=usage_text)
+
+    parser.add_argument("-i", "--input", dest="input", metavar='FILE', required=True, help="Input path contained in a text file.")
+    parser.add_argument("-o", "--output", dest="output", metavar='FILE', required=True, help="Output file to save the ParaView state in (*.pvsm)")
+
+    args = parser.parse_args(argv)
+
+    if not argv:
+        parser.print_help()
+        return
+
+    if not args.input:
+       print('Need an input file')
+       parser.print_help()
+       sys.exit(1)
+
+    if not args.output:
+       print('Need an output file')
+       parser.print_help()
+       sys.exit(1)
+
+    ## read pvsm
+    pvs.LoadState(args.input)
+
+    ## read a vtp
+    #reader = pvs.XMLPolyDataReader(FileName=args.input)
+
+    ## read a MetaImage (there is a MetaImageWriter but no MetaImageReader)
+    #reader = pvs.OpenDataFile(args.input)
+
+
+
+    if args.output:
+        try:
+            f = open(args.output, 'w')
+            f.close()
+            ok = True
+        except:
+            print("Cannot save to path %r" % save_path)
+
+            import traceback
+            traceback.print_exc()
+
+        if ok:
+            pvs.servermanager.SaveState(args.output)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
